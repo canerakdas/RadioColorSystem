589a4006fe18010774382f0af02500f9712fe87a
Caner Akdas <canerakdas@gmail.com>
2023-04-05 16:40:23 +0300

    refactor: svelte kit library folder structure

---
+++ src/lib/components/RadioActive.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+
+  import radioColor, {getImageColor} from '../package/index';
+  import type {ColorOptions} from '../package/index.type';
+
+  export let target = '';
+  export let async = false;
+  export let color: ColorOptions = {
+    color: {h: 0, s: 0, l: 50},
+    name: 'dynamic',
+  };
+
+  const {setTarget, setColors, stylesheet} = radioColor();
+  let style = '';
+  let mounted = false;
+
+  if (color.name !== undefined) {
+    setTarget(`#${color.name}`);
+  }
+
+  if (async === false) {
+    setColors([color]);
+    style = stylesheet();
+  }
+
+  $: if (mounted) {
+    getImageColor({
+      name: color.name || '',
+      target: target,
+      callback: (c: ColorOptions) => {
+        setColors([{...color, ...{color: c.color}}]);
+        style = stylesheet();
+      },
+    });
+  }
+
+  onMount(() => {
+    mounted = true;
+  });
+</script>
+
+<svelte:element this="style">{style}</svelte:element>
+<div id={color.name}>
+  <slot />
+</div>
---
+++ src/lib/package/index.ts
@@ -0,0 +1,241 @@
+/**
+ * @fileoverview This module exports a `radioColor` module, which is responsible
+ * for generating a set of CSS color tokens based on the given options.
+ */
+
+import {generate} from 'css-tree';
+
+/* Utils */
+import colorRange from './utils/range';
+import {getTextColor, variants} from './utils/helpers';
+import {getImageColor} from './utils/image';
+import toCssTree from './utils/cssTree';
+import cssToken from './utils/token';
+
+/* Type imports */
+import type {
+  TokenScheme,
+  ColorOptions,
+  Rule,
+  TokenTheme,
+  TokenNames,
+} from './index.type';
+
+/**
+ * @typedef {Object} TokenTheme
+ * @property {function} TokenTheme - Returns an array of CSS declaration objects.
+ * @property {Color} TokenTheme.color - The color to use for the token.
+ * @property {string} TokenTheme.name - The name of the token.
+ * @property {boolean} TokenTheme.font - Whether to generate a font color token.
+ * @property {boolean} TokenTheme.dark - Whether to generate a dark color token.
+ * @property {boolean} TokenTheme.light - Whether to generate a light color token.
+ * @property {string} TokenTheme.target - The target selector for the token.
+ */
+const radioColor = function () {
+  const attributes: Rule[] = [];
+  const classes: Rule[] = [];
+  const tokens: TokenScheme = {
+    light: [],
+    dark: [],
+  };
+
+  let styles = '';
+  let target = ':root';
+
+  /**
+   * Sets the colors for the color system
+   * @param {ColorOptions[]} colorsOptions - An array of color options
+   * @returns {void}
+   */
+  const setColors = (colorsOptions: ColorOptions[]): void => {
+    for (const {
+      prefix = '',
+      color,
+      name = 'primary',
+      suffix = '',
+      dark = true,
+      font = true,
+      selector = {attribute: true, class: true},
+      theme = {
+        darken: variants.dark,
+        lighten: variants.light,
+      },
+    } of colorsOptions) {
+      const {darken = variants.dark, lighten = variants.light} = theme;
+
+      const ranges = {
+        light: colorRange(lighten(color)),
+        dark: colorRange(darken(color)),
+      };
+
+      for (let i = 0; i < ranges.light.length; i++) {
+        const lightSegment = ranges.light[i];
+        const darkSegment = ranges.dark[i];
+
+        const names = {
+          text: `${prefix}${name}-font-${i * 10}${suffix}`,
+          background: `${prefix}${name}-${i * 10}${suffix}`,
+        };
+
+        setTokens(
+          {dark: dark ? darkSegment : null, light: lightSegment, font},
+          names
+        );
+
+        if (selector.attribute) setAttributes(names, font, selector.attribute);
+        if (selector.class) setClasses(names, font, selector.class);
+      }
+    }
+
+    const {light, dark} = toCssTree.tokens.scheme;
+
+    styles = generate({
+      type: 'StyleSheet',
+      // TODO: Update the used tree, or clone it before using it.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      children: [
+        light(tokens.light, target),
+        dark(tokens.dark, target),
+        ...classes,
+        ...attributes,
+      ],
+    });
+  };
+
+  /**
+   * Sets the tokens for the color system
+   * @param {TokenTheme} theme - The color theme
+   * @param {TokenNames} tokenNames - The names of the tokens
+   * @returns {void}
+   */
+  const setTokens = (
+    theme: TokenTheme,
+    {background, text}: TokenNames
+  ): void => {
+    if (theme.dark) {
+      const {light, dark} = cssToken(background, {
+        light: theme.light,
+        dark: theme.dark,
+      });
+
+      tokens.light = [...tokens.light, ...light];
+      if (dark) tokens.dark = [...tokens.dark, ...dark];
+    } else {
+      const {light} = cssToken(background, {
+        light: theme.light,
+      });
+
+      tokens.light = [...tokens.light, ...light];
+    }
+
+    if (theme.font) {
+      if (theme.dark) {
+        const {light, dark} = cssToken(text, {
+          light: getTextColor(theme.light),
+          dark: getTextColor(theme.dark),
+        });
+
+        tokens.light = [...tokens.light, ...light];
+        if (dark) tokens.dark = [...tokens.dark, ...dark];
+      } else {
+        const {light} = cssToken(text, {
+          light: getTextColor(theme.light),
+        });
+
+        tokens.light = [...tokens.light, ...light];
+      }
+    }
+  };
+
+  /**
+   * Sets the css attributes
+   * @param {TokenNames} tokenNames - The names of the tokens
+   * @param {boolean} font - If the font color should be set
+   * @param {boolean} selector - CSS tree selector type
+   * @returns {void}
+   */
+  const setAttributes = (
+    {background, text}: TokenNames,
+    font: boolean,
+    selector?: boolean
+  ): void => {
+    if (selector) {
+      attributes.push(
+        toCssTree.selector(
+          {
+            type: 'Identifier',
+            name: background,
+          },
+          'background-color',
+          'AttributeSelector'
+        )
+      );
+
+      if (font) {
+        attributes.push(
+          toCssTree.selector(
+            {
+              type: 'Identifier',
+              name: text,
+            },
+            'color',
+            'AttributeSelector'
+          )
+        );
+      }
+    }
+  };
+
+  /**
+   * Sets the css classes
+   * @param {TokenNames} tokenNames - The names of the tokens
+   * @param {boolean} font - If the font color should be set
+   * @param {boolean} selector - CSS tree selector type
+   * @returns {void}
+   */
+  const setClasses = (
+    {background, text}: TokenNames,
+    font: boolean,
+    selector?: boolean
+  ): void => {
+    if (selector) {
+      classes.push(
+        toCssTree.selector(background, 'background-color', 'ClassSelector')
+      );
+
+      if (font) {
+        classes.push(toCssTree.selector(text, 'color', 'ClassSelector'));
+      }
+    }
+  };
+
+  /**
+   * Returns the stylesheet
+   * @returns {string} - The stylesheet
+   */
+  const stylesheet = (): string => {
+    return styles;
+  };
+
+  /**
+   * Sets the target for the stylesheet
+   * @param {string} t - The target
+   * @returns {void}
+   */
+  const setTarget = (t: string): void => {
+    target = t;
+  };
+
+  return {
+    setColors,
+    setTokens,
+    setTarget,
+    setAttributes,
+    stylesheet,
+  };
+};
+
+export default radioColor;
+
+export {getImageColor};
